5a187e497f5df2a2b1ff4dd3fcb0c5e8dc8a1595
Jeremy <jeremy.geneste@gmail.com>
2025-08-22 15:26:57 +0200

    feat: control data caching

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,11 +1,17 @@
-import Messages from '@/components/messages';
+import { unstable_noStore } from "next/cache";
+
+import Messages from "@/components/messages";
+
+// Cache the page for 5 seconds
+//export const revalidate = 5;
+
+// Force the page to be dynamic (no caching)
+// export const dynamic = "force-dynamic";
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
+  // Prevent caching for this fetch request
+  unstable_noStore();
+  const response = await fetch("http://localhost:8080/messages");
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
